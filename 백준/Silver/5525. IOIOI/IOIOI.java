f19233f97b7a4628e3b138e1f6c7a76e19166895
YongHo Shin <gdtknight@naver.com>
2023-05-23 16:46:26 +0900

    [Silver I] Title: IOIOI, Time: 144 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    int sLen = Integer.parseInt(br.readLine());
+    String S = br.readLine();
+
+    String pN = "I" + "OI".repeat(N);
+
+    int idx = S.indexOf(pN);
+    int answer = 0;
+    while (idx != -1) {
+      answer++;
+      idx = S.indexOf(pN, idx + 1);
+    }
+    System.out.println(answer);
+  }
+}
