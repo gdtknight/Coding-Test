7e3a171b413ba36db180a275ebb83ae57d2e3881
YongHo Shin <gdtknight@naver.com>
2023-05-23 17:34:55 +0900

    Update IOIOI.java -> 100점 맞았음 !!!!
    문제 다시 풀어서 100점 맞았음 !!!!

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,18 +6,30 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    int answer = 0;
     int N = Integer.parseInt(br.readLine());
     int sLen = Integer.parseInt(br.readLine());
     String S = br.readLine();
 
-    String pN = "I" + "OI".repeat(N);
+    for (int startIdx = 0; startIdx < S.length(); startIdx++) {
+      if (S.charAt(startIdx) == 'I') {
+        int count = 0;
+        for (int idx = startIdx + 1; idx < S.length() - 1; idx += 2) {
+          if (S.charAt(idx) == 'O' && S.charAt(idx + 1) == 'I') {
+            count++;
+          } else {
+            break;
+          }
+        }
 
-    int idx = S.indexOf(pN);
-    int answer = 0;
-    while (idx != -1) {
-      answer++;
-      idx = S.indexOf(pN, idx + 2);
+        if (count >= N) {
+          answer += (count - N + 1);
+        }
+
+        startIdx = startIdx + count * 2;
+      }
     }
+
     System.out.println(answer);
   }
 }
