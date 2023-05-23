5f97d715b5b7dc393988a3954cd8cf532a97fab6
YongHo Shin <gdtknight@naver.com>
2023-05-23 09:09:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 320 ms, Memory: 23216 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,25 +1,96 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (i1,i2) -> 
-            Integer.compare(Math.abs(i1),Math.abs(i2)) == 0
-            ? Integer.compare(i1,i2)
-            : Integer.compare(Math.abs(i1),Math.abs(i2)) 
-        );
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) {
-                num = pq.isEmpty() ? 0 : pq.poll();
-                System.out.println(num);
-            } else {
-                pq.offer(num);
-            }
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    MinAbsHeap minAbsHeap = new MinAbsHeap(N);
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < N; i++) {
+      int num = Integer.parseInt(br.readLine());
+      if (num == 0) {
+        sb.append(minAbsHeap.pop()).append("\n");
+      } else {
+        minAbsHeap.push(num);
+      }
     }
+
+    System.out.println(sb);
+  }
+
+  static class MinAbsHeap {
+    int[] minHeap;
+    int size;
+
+    MinAbsHeap(int defaultSize) {
+      this.size = 0;
+      this.minHeap = new int[defaultSize * 2];
+    }
+
+    public void push(int x) {
+      if (size == 0) {
+        minHeap[++size] = x;
+        return;
+      }
+
+      minHeap[++size] = x;
+      int curPos = size;
+      while (curPos > 1) {
+        int parent = curPos / 2;
+        if (Math.abs(minHeap[parent]) > Math.abs(minHeap[curPos])) swap(parent, curPos);
+        else if (Math.abs(minHeap[parent]) == Math.abs(minHeap[curPos])
+            && minHeap[parent] > minHeap[curPos]) swap(parent, curPos);
+        curPos = parent;
+      }
+    }
+
+    public int pop() {
+      if (size == 0) {
+        return 0;
+      }
+
+      int data = minHeap[1];
+      minHeap[1] = minHeap[size--];
+      int curPos = 1;
+      while (curPos <= size) {
+        int left = curPos * 2;
+        int right = curPos * 2 + 1;
+        if (right <= size) {
+          int nextPos = left;
+          if (Math.abs(minHeap[left]) < Math.abs(minHeap[right])) {
+            nextPos = left;
+          } else if (Math.abs(minHeap[left]) > Math.abs(minHeap[right])) {
+            nextPos = right;
+          } else if (minHeap[left] < minHeap[right]) {
+            nextPos = left;
+          } else if (minHeap[left] > minHeap[right]) {
+            nextPos = right;
+          }
+          if (Math.abs(minHeap[curPos]) > Math.abs(minHeap[nextPos])) swap(curPos, nextPos);
+          else if (Math.abs(minHeap[curPos]) == Math.abs(minHeap[nextPos])
+              && minHeap[curPos] > minHeap[nextPos]) swap(curPos, nextPos);
+          curPos = nextPos;
+        } else if (left <= size) {
+          if (Math.abs(minHeap[curPos]) > Math.abs(minHeap[left])) swap(curPos, left);
+          else if (Math.abs(minHeap[curPos]) == Math.abs(minHeap[left])
+              && minHeap[curPos] > minHeap[left]) swap(curPos, left);
+          curPos = left;
+        } else {
+          break;
+        }
+      }
+
+      return data;
+    }
+
+    private void swap(int idx1, int idx2) {
+      int temp = minHeap[idx1];
+      minHeap[idx1] = minHeap[idx2];
+      minHeap[idx2] = temp;
+    }
+  }
 }
