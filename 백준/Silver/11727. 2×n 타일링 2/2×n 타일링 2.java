492a5c9151eff0631ad702c4f18cd3e3f1814457
YongHo Shin <gdtknight@naver.com>
2023-05-26 13:34:23 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int[] answer = new int[1001];
+    answer[0] = 0;
+    answer[1] = 1;
+    answer[2] = 3;
+    for (int i = 3; i<= 1000; i++) {
+      answer[i] = (answer[i-1] + answer[i-2]*2) % 10007;
+    }
+
+    System.out.println(answer[N]);
+
+    br.close();
+  }
+
+}
