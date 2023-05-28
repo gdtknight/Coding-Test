a25c4cc83d722549fd9f88dfaf59f050aea77bf1
YongHo Shin <gdtknight@naver.com>
2023-05-28 15:49:09 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    long[][] result = new long[N + 1][2];
+
+    result[1][0] = 0;
+    result[1][1] = 1;
+
+    for (int i = 2; i <= N; i++) {
+      result[i][0] = result[i - 1][0] + result[i - 1][1];
+      result[i][1] = result[i - 1][0];
+    }
+
+    System.out.println(result[N][0] + result[N][1]);
+
+    br.close();
+  }
+}
