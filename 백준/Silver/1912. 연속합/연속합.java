ac1282a091075ea9d79613ef34de290594423dc6
YongHo Shin <gdtknight@naver.com>
2023-05-31 12:39:49 +0900

    [Silver II] Title: 연속합, Time: 268 ms, Memory: 23380 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int cur;
+    int curMax = 0;
+    int max = 0;
+
+    int maxNum = Integer.MIN_VALUE;
+    for (int i = 0; i < N; i++) {
+      cur = Integer.parseInt(st.nextToken());
+      if (maxNum < cur) maxNum = cur;
+      curMax = Math.max(curMax + cur, cur);
+      max = Math.max(max, curMax);
+    }
+    if (max == 0) max = maxNum;
+
+    System.out.println(max);
+  }
+}
