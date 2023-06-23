6deb9983b73de817510464c95b45e79f61feb89d
YongHo Shin <gdtknight@naver.com>
2023-06-23 15:13:25 +0900

    [Silver I] Title: 스타트링크, Time: 220 ms, Memory: 62228 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -15,14 +15,17 @@ public class Main {
     int U = Integer.parseInt(st.nextToken()); // 엘리베이터 버튼
     int D = Integer.parseInt(st.nextToken()); // 엘리베이터 버튼
 
-    Set<Integer> set = new HashSet<>(); // 방문한 층 저장
+    boolean[] visited = new boolean[F + 1];
+
     Queue<int[]> queue = new LinkedList<>();
 
-    set.add(S);
+    visited[S] = true;
+
     queue.add(new int[] {S, 0});
 
     while (!queue.isEmpty()) {
       int[] floor = queue.poll(); // 현재 있는 층
+
       if (floor[0] == G) {
         System.out.println(floor[1]);
         return;
@@ -31,11 +34,13 @@ public class Main {
       int nextU = floor[0] + U;
       int nextD = floor[0] - D;
 
-      if (nextU <= F && set.add(nextU)) {
+      if (nextU <= F && !visited[nextU]) {
+        visited[nextU] = true;
         queue.offer(new int[] {nextU, floor[1] + 1});
       }
 
-      if (nextD >= 1 && set.add(nextD)) {
+      if (nextD >= 1 && !visited[nextD]) {
+        visited[nextD] = true;
         queue.offer(new int[] {nextD, floor[1] + 1});
       }
     }
