13691e64ebd23c165c8535cddee667a4733bf015
YongHo Shin <gdtknight@naver.com>
2023-11-23 09:07:27 +0900

    [level 2] Title: 타겟 넘버, Time: 5.63 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        
+        Stack<Sum> stack = new Stack<>();
+        Sum cur = new Sum(0, numbers[0]);
+        stack.push(cur);
+        cur = new Sum(0, -numbers[0]);
+        stack.push(cur);
+        
+        
+        while(!stack.isEmpty()) {
+            cur = stack.pop();
+            
+            if (cur.getIdx() != numbers.length - 1) {
+                stack.push(new Sum(cur.getIdx() + 1, cur.getSum() + numbers[cur.getIdx() + 1]));
+                stack.push(new Sum(cur.getIdx() + 1, cur.getSum() - numbers[cur.getIdx() + 1]));
+                continue;
+            }
+            
+            if (cur.getIdx() == numbers.length - 1 && cur.getSum() == target) {
+                answer++; 
+            }
+            
+        }
+        
+        return answer;
+    }
+}
+
+class Sum {
+    private int idx;
+    private int sum;
+    
+    public Sum(int idx, int sum) {
+        this.idx = idx;
+        this.sum = sum;
+    }
+    
+    public int getIdx() {
+        return this.idx;
+    }
+    
+    public int getSum() {
+        return this.sum;
+    }
+}
