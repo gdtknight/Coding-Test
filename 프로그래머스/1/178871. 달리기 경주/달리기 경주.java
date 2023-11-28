e27744493dce79d64bc2ee6c2669b955fab74db6
YongHo Shin <gdtknight@naver.com>
2023-11-28 14:47:12 +0900

    [level 1] Title: 달리기 경주, Time: 0.08 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> rankMap = new HashMap<>();
+ 
+        for (int i = 0; i < players.length; i++) {
+            rankMap.put(players[i], i);
+        }
+    
+        for (String called : callings) {
+            int rank = rankMap.get(called);
+            
+            String player = players[rank - 1];
+            players[rank - 1] = players[rank];
+            players[rank] = player;
+            
+            rankMap.put(players[rank - 1], rank - 1);
+            rankMap.put(players[rank], rank);
+        }
+        
+        return players;
+    }
+}
