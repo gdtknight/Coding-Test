fca9f052fbfcbe93a1c8179a411ece3413ed6821
YongHo Shin <gdtknight@naver.com>
2024-01-11 10:21:27 +0900

    [level unrated] Title: OX퀴즈, Time: 2.42 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.java
@@ -2,31 +2,25 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] quiz) {
-        String[] answer = new String[quiz.length];
-        
-        for (int idx = 0 ; idx < quiz.length; idx++) {
-            String[] args = quiz[idx].split("=");
-            
-            args[0] = args[0].trim();
-            args[1] = args[1].trim();
-            
-            String[] leftArgs = args[0].split(" [+-] ");
-            
-            int leftResult = 0;
-            if (args[0].contains("+")) {
-                leftResult = Integer.parseInt(leftArgs[0]) + Integer.parseInt(leftArgs[1]);
-            } else if (args[0].contains("-")) {
-                leftResult = Integer.parseInt(leftArgs[0]) - Integer.parseInt(leftArgs[1]);
-            } else {
-                throw new RuntimeException("Fxxk");  
-            }
-            
-            int rightResult = Integer.parseInt(args[1]);
-            
-            if (leftResult == rightResult) answer[idx] = "O";
-            else answer[idx] = "X";
-        }
-        
-        return answer;
+        return Arrays.stream(quiz)
+            .map(expr -> expr.split(" = "))
+            .map(args -> {
+                int leftResult = 0;
+                int rightResult = Integer.parseInt(args[1]);
+                
+                String[] leftArgs = args[0].split(" [+-] ");
+                if (args[0].contains(" + ")) {
+                    leftResult = Integer.parseInt(leftArgs[0]) + Integer.parseInt(leftArgs[1]);
+                } else if (args[0].contains(" - ")) {
+                    leftResult = Integer.parseInt(leftArgs[0]) - Integer.parseInt(leftArgs[1]);
+                }
+                
+                if (leftResult == rightResult) {
+                    return "O";
+                } 
+                
+                return "X";
+            })
+            .toArray(String[]::new);
     }
 }
