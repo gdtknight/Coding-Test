ce4528f5af365bd888ad67811e2c9005e9a7b80f
YongHo Shin <gdtknight@naver.com>
2024-01-13 10:07:11 +0900

    [level Lv.2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        Set<String> used = new HashSet<>();
+        used.add(words[0]);
+        
+        for (int idx = 1; idx < words.length; idx++) {
+            String prevStr = words[idx - 1];
+            char prevChar = prevStr.charAt(prevStr.length() - 1);
+            String curStr = words[idx];
+            char curChar = curStr.charAt(0);
+            
+            if (!used.add(words[idx]) || prevChar != curChar)  {
+                return new int[]{idx % n + 1, idx / n + 1};
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
