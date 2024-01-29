077f524eff92fd37ef66e9cecec616a08cb7a8fc
YongHo Shin <gdtknight@naver.com>
2024-01-29 10:03:42 +0900

    [level 2] Title: 기능개발, Time: 2.21 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int NUM_OF_WORKS = progresses.length;
+        
+        int[] days = new int[NUM_OF_WORKS];
+        
+        for (int idx = 0; idx < NUM_OF_WORKS; idx++) {
+            days[idx] = Double.valueOf(
+                Math.ceil((100.0 - progresses[idx]) / speeds[idx]))
+                .intValue();
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        
+        int lastIdx = 0;
+        
+        for (int idx = 1; idx < NUM_OF_WORKS; idx++) {
+            if (days[lastIdx] < days[idx]) {
+                stack.push(1);
+                lastIdx = idx;
+            } else {
+                stack.push(stack.pop() + 1);
+            }
+        }
+        
+        return stack.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}
