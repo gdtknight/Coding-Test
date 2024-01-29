da1344d0151248bec392cca741d4ee774a1843dd
YongHo Shin <gdtknight@naver.com>
2024-01-29 18:48:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.21 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,38 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        Deque<Integer> deque = new LinkedList<>();
+        
+        int[] answer = {};
+        
+        for (String operation : operations) {
+            String[] args = operation.split(" ");
+            
+            String op = args[0];
+            int arg = Integer.parseInt(args[1]);
+            
+            if ("I".equals(op)) {
+                if (deque.isEmpty()) {
+                    deque.offer(arg);
+                } else if (arg <= deque.peekFirst()){
+                    deque.offerFirst(arg);
+                } else if (arg >= deque.peekLast()){
+                    deque.offerLast(arg);
+                }
+            } else if ("D".equals(op)) {
+                if (arg == -1 && !deque.isEmpty()) {
+                    deque.pollFirst();
+                } else if (arg == 1 && !deque.isEmpty()) {
+                    deque.pollLast();
+                }
+            }
+        }
+        
+        return deque.isEmpty()
+            ? new int[]{0, 0}
+            : new int[]{deque.peekLast(), deque.peekFirst()};
+    }
+}
+
