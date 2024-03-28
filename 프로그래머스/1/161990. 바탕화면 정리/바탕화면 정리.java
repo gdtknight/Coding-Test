3cbf835faf531f5326f272f443717648a13d39e9
YongHo Shin <gdtknight@naver.com>
2024-03-28 20:56:11 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.28 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int minRow = Integer.MAX_VALUE, maxRow = 0;
-        int minCol = Integer.MAX_VALUE, maxCol = 0;
+        int minRow = Integer.MAX_VALUE;
+        int minCol = Integer.MAX_VALUE;
+        int maxRow = 0;
+        int maxCol = 0;
         
         for (int row = 0; row < wallpaper.length; row++) {
             for (int col = 0; col < wallpaper[0].length(); col++) {
-                if (wallpaper[row].charAt(col) != '#') continue;
+                if (wallpaper[row].charAt(col) != '#') {
+                    continue;  
+                }
                 
                 minRow = Math.min(minRow, row);
                 maxRow = Math.max(maxRow, row);
                 
                 minCol = Math.min(minCol, col);
                 maxCol = Math.max(maxCol, col);
-                
             }
         }
         
