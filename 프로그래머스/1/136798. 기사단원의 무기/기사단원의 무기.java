65805df8873df77591be69e8618fe0f8976d484f
YongHo Shin <gdtknight@naver.com>
2024-04-03 13:33:47 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.23 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,17 +2,34 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int number, int limit, int power) {
-        int[] countArr = new int[number + 1];
+        int answer = 0;
         
-        for (int idx = 1; idx <= number; idx++) {
-            for (int j = idx; j <= number; j += idx) {
-                countArr[j]++;
+        /**
+        *
+        * 메인 아이디어
+        * : 어떤 숫자 n의 약수가 x라면 n은 x의 배수
+        *   즉, n이 x의 배수라면 n의 약수의 개수를 하나씩 추가한다.
+        *
+        **/
+        
+        // 1부터 number 까지의 약수의 개수를 저장
+        int[] countDiv = new int[number + 1];
+        
+        // n 은 1부터 number 까지
+        for (int n = 1; n <= number; n++) {
+            // x 는 n 의 모든 배수
+            for (int x = n; x <= number; x += n) {
+                countDiv[x]++;
             }
+            
+            if (countDiv[n] > limit) {
+                countDiv[n] = power;
+            }
+            
+            answer += countDiv[n];
         }
         
-        return Arrays.stream(countArr)
-            .map(cnt -> cnt > limit ? power : cnt)
-            .reduce((n1, n2) -> n1 + n2)
-            .orElse(0);
+        // 약수의 개수, 즉, 공격력이 power 를 초과할 경우 power 로 변환
+        return answer;
     }
 }
