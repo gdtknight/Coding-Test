78fb043e72bc4e80c792f47061cf55506003df2c
YongHo Shin <gdtknight@naver.com>
2024-04-03 14:03:14 +0900

    [level 1] Title: 옹알이 (2), Time: 0.92 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,88 +1,12 @@
-import java.util.Map;
-import java.util.HashMap;
-
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
-        Trie trie = new Trie();
-        
-        trie.insert("aya");
-        trie.insert("ye");
-        trie.insert("woo");
-        trie.insert("ma");
-        
-        outer :
-        for (String word : babbling) {
-            String prev = "";
-            
-            StringBuilder sb = new StringBuilder();
-            Node node = trie.rootNode;
-            
-            for (int idx = 0; idx < word.length(); idx++) {
-                node = node.childNodes.getOrDefault(word.charAt(idx), null);
-                
-                // 해당 문자로 시작하는 발음이 없는 경우
-                if (node == null) {
-                    continue outer;
-                }
-                
-                sb.append(word.charAt(idx));
-                
-                if (node.endOfWord) {
-                    String curWord = sb.toString();
-                    System.out.println(curWord);
-                    
-                    // 같은 발음이 연속해서 나오는 경우
-                    if (prev.equals(curWord)) {
-                        continue outer;
-                    }
-                    
-                    if (idx == word.length() - 1) {
-                        answer++;  
-                    }
-                    
-                    prev = curWord;
-                    sb = new StringBuilder();
-                    node = trie.rootNode;
-                    
-                }
-            }
+        for(String str:babbling){
+            if(str.contains("ayaaya") || str.contains("yeye") || str.contains("woowoo") || str.contains("mama"))
+                continue;
+            if(str.replace("aya", "1").replace("ye", "1").replace("woo", "1").replace("ma", "1").replace("1", "").length() == 0)
+                answer++;
         }
-        
         return answer;
-    }
-    
-    class Trie {
-        Node rootNode = new Node();
-        
-        void insert(String str) {
-            Node node = this.rootNode;
-            
-            for (char c : str.toCharArray()) {
-                node = node.childNodes.computeIfAbsent(c, key -> new Node());
-            }
-            
-            node.endOfWord = true;
-        }
-        
-        boolean search(String str) {
-            Node node = this.rootNode;
-            
-            for (char c : str.toCharArray()) {
-                node = node.childNodes.getOrDefault(c, null);
-                
-                if (node == null) {
-                    return false;
-                }
-            }
-            
-            return node.endOfWord;
-        }
-    }
-    
-    class Node {
-        Map<Character, Node> childNodes = new HashMap<>();
-        boolean endOfWord = false;
     }
 }
