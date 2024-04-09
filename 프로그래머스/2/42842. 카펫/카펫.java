4892273d9838e426beddc17e95a9051a3af99bcc
YongHo Shin <gdtknight@naver.com>
2024-04-09 19:53:49 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,29 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        /**
+        *
+        * 메인 아이디어 
+        *  : 가운데 노란색 사각형의 영역을 기준으로 가로(w), 세로(h) 계산
+        *  테두리를 포함한 전체 사각형의 넓이는 (w + 2) * (h + 2)
+        *
+        **/ 
         
         int[] ans = new int[]{};
         
-        if (yellow == 1) return new int[]{yellow + 2, yellow + 2};
+        final int MAX_WIDTH = yellow / 2 + 1;
         
-        for (int w = 1; w <= yellow / 2; w++) {
-            if (yellow % w == 0) {
-                int h = yellow / w;
-                if ((w + 2) * (h + 2) == (brown + yellow)) {
-                    return (w + 2) >= (h + 2)
-                        ? new int[]{w + 2, h + 2}
-                        : new int[]{h + 2, w + 2};
-                }
+        for (int w = 1; (w <= MAX_WIDTH); w++) {
+            if (yellow % w != 0) continue;
+            
+            int h = yellow / w;
+            
+            if ((w + 2) * (h + 2) != (brown + yellow)) {
+                continue;
             }
+            
+            return (w + 2) >= (h + 2)
+                ? new int[]{w + 2, h + 2}
+                : new int[]{h + 2, w + 2};
         }
         
         return ans;
