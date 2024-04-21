0cc9f2d033ed84791421b7a2ca88cfdd5f71cefc
YongHo Shin <gdtknight@naver.com>
2024-04-21 20:39:39 +0900

    [level 1] Title: 카드 뭉치, Time: 2.37 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,15 +1,17 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int idx1 = 0;
-        int idx2 = 0;
+        int idx1 = 0, idx2 = 0;
         
         for (String word : goal) {
-            if (idx1 < cards1.length && word.equals(cards1[idx1])) 
+            if (idx1 < cards1.length && word.equals(cards1[idx1])) {
+                System.out.print(cards1[idx1] + " ");
                 idx1++;
-            else if (idx2 < cards2.length && word.equals(cards2[idx2])) 
+            } else if (idx2 < cards2.length && word.equals(cards2[idx2])) {
+                System.out.print(cards2[idx2] + " ");
                 idx2++;
-            else 
+            } else {
                 return "No";
+            }
         }
         
         return "Yes";
