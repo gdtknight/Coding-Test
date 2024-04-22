9adc08aefa041904ccf93d462fae99d7b9767bb3
YongHo Shin <gdtknight@naver.com>
2024-04-22 14:53:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.60 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,81 +1,60 @@
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.LinkedList;
 
 class Solution {
-    static final int[][] dirs = new int[][]{
-        {0, 1},
-        {0, -1},
-        {1, 0},
-        {-1, 0}
+    static final int[][] DIRS = new int[][] {
+        { 0,  1}, // 동
+        { 0, -1}, // 서
+        {-1,  0}, // 남
+        { 1,  0}  // 북
     };
     
     // maps = n x m
     public int solution(int[][] maps) {
         int answer = -1;
-        int n = maps.length;
-        int m = maps[0].length;
         
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        Queue<int[]> queue = new LinkedList<>();
         
-        Queue<Pos> queue = new LinkedList<>();
+        final int ROWS = maps.length;
+        final int COLS = maps[0].length;
         
-        queue.offer(new Pos(0, 0, 1));
+        boolean[][] visited = new boolean[ROWS][COLS];
+        
+        queue.offer(new int[]{0, 0});
+        
         visited[0][0] = true;
         
         outer:
         while (!queue.isEmpty()) {
-            Pos cur = queue.poll();
-            for (int[] dir : dirs) {
-                int nextX = cur.getX() + dir[0];
-                int nextY = cur.getY() + dir[1];
-                int nextCount = cur.getCount() + 1;
+            int[] cur = queue.poll();
+            
+            int row = cur[0];
+            int col = cur[1];
+            
+            for (int[] dir : DIRS) {
+                int nextRow = cur[0] + dir[0];
+                int nextCol = cur[1] + dir[1];
                 
-                if (nextX < 0 
-                    || nextY < 0 
-                    || nextX >= maps.length 
-                    || nextY >= maps[0].length
-                    || maps[nextX][nextY] == 0) {
+                if (nextRow < 0
+                    || nextCol < 0
+                    || ROWS <= nextRow
+                    || COLS <= nextCol
+                    || maps[nextRow][nextCol] == 0
+                    || visited[nextRow][nextCol]
+                   ) {
                     continue;
                 }
                 
-                
-                if (nextX == n-1 && nextY == m-1) {
-                    answer = nextCount;
-                    break outer;
+                if (nextRow == (ROWS - 1) && nextCol == (COLS - 1)) {
+                    return maps[row][col] + 1;
                 }
                 
-                if(!visited[nextX][nextY]) {
-                    visited[nextX][nextY] = true;
-                    queue.offer(new Pos(nextX, nextY, nextCount));
-                }
+                visited[nextRow][nextCol] = true;
+                queue.offer(new int[]{nextRow, nextCol});
+                maps[nextRow][nextCol] = maps[row][col] + 1;
             }
         }
         
-        return answer;
-    }
-}
-
-class Pos {
-    private int x;
-    private int y;
-    private int count;
-    
-    public Pos(int x, int y, int count) {
-        this.x = x;
-        this.y = y;
-        this.count = count;
-    }
-    
-    public int getX() {
-        return this.x;
-    }
-    
-    public int getY() {
-        return this.y;
-    }
-    
-    public int getCount() {
-        return this.count;
+        return -1;
     }
 }
