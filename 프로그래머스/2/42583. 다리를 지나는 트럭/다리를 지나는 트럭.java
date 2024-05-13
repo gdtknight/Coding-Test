40ff4e7dcd5e63437e0ab15cb356f5837b0fb8b0
YongHo Shin <gdtknight@naver.com>
2024-05-13 23:36:06 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.42 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -8,33 +8,61 @@ class Solution {
         final int MAX_WEIGHT,
         final int[] WEIGHTS
     ) {
-        Queue<Truck> bridge = new LinkedList<>();
         
-        int currentTime = 0;
+        int currentTime = 1;
         int currentWeight = 0;
         
-        boolean flag = true;
-        for (int weight : WEIGHTS) {
-            while (currentWeight + weight > MAX_WEIGHT) {
-                if (bridge.peek().getEntryTime() + BRIDGE_LENGTH >= currentTime++) {
-                    Truck truck = bridge.poll();
-                    currentWeight -= truck.getWeight();
-                    flag = false;
-                }
-            }
-            
-            if (flag) {
+        int idx = 0;
+        
+        int weight = Integer.MAX_VALUE;
+        
+        Truck truck = null;
+        
+        Queue<Truck> bridge = new LinkedList<>();
+        
+        while (idx < WEIGHTS.length) {
+            // 다리를 건너는 트럭이 없으면 바로 건너면 됨.
+            if (bridge.isEmpty()) {
+                bridge.offer(new Truck(WEIGHTS[idx], currentTime));
+                currentWeight += WEIGHTS[idx];
                 currentTime++;
-            } else {
-                flag = true;
+                idx++;
+                continue;
             }
             
-            bridge.offer(Truck.create(weight, currentTime));
-            currentWeight += weight;
+            // 다리 위에 트럭이 있는 경우 
+            
+            // 도착한 트럭
+            if (bridge.peek().getEntryTime() + BRIDGE_LENGTH == currentTime) {
+                truck = bridge.poll();
+                currentWeight -= truck.getWeight();
+            }
+                
+            
+            // 다리를 건널 수 있는 경우
+            if (currentWeight + WEIGHTS[idx] <= MAX_WEIGHT) {
+                bridge.offer(new Truck(WEIGHTS[idx], currentTime));
+                currentWeight += WEIGHTS[idx];
+                currentTime++;
+                idx++;
+            }
+            
+            // 다리를 건널 수 없는 경우 앞선 트럭이 다리를 건널 때까지 시간을 흘려보냄.
+            else {
+                currentTime++;
+            }
         }
         
+        // 모든 트럭이 다리를 건널 때까지 시간 계산
+        while (!bridge.isEmpty()) {
+            if (bridge.peek().getEntryTime() + BRIDGE_LENGTH == currentTime) {
+                truck = bridge.poll();
+                currentWeight -= truck.getWeight();
+            }
+            currentTime++;
+        }
         
-        return currentTime + BRIDGE_LENGTH;
+        return currentTime - 1;
     }
 }
 
@@ -42,13 +70,9 @@ class Truck {
     final int WEIGHT;
     final int ENTRY_TIME;
     
-    private Truck(final int WEIGHT, final int ENTRY_TIME) {
+    public Truck(final int WEIGHT, final int ENTRY_TIME) {
         this.WEIGHT = WEIGHT;
         this.ENTRY_TIME = ENTRY_TIME;
-    }
-    
-    public static Truck create(final int WEIGHT, final int ENTRY_TIME) {
-        return new Truck(WEIGHT, ENTRY_TIME);
     }
     
     public int getWeight() {
@@ -57,5 +81,9 @@ class Truck {
     
     public int getEntryTime() {
         return ENTRY_TIME;
+    }
+    
+    public String toString() {
+        return "{w:" + WEIGHT + ",t:" + ENTRY_TIME + "}";
     }
 }
