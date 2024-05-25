b66e066e119e3990f2c19642504fc0e20314231c
YongHo Shin <gdtknight@naver.com>
2024-05-25 20:04:17 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.54 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,11 +1,6 @@
-import java.util.Deque;
-import java.util.LinkedList;
-
 class Solution {
     public int[] solution(String[] operations) {
-        Deque<Integer> deque = new LinkedList<>();
-        
-        int[] answer = {};
+        DoublePriorityQueue dpq = new DoublePriorityQueue();
         
         for (String operation : operations) {
             String[] args = operation.split(" ");
@@ -14,18 +9,116 @@ class Solution {
             int arg = Integer.parseInt(args[1]);
             
             if ("I".equals(op)) {
-                if (deque.isEmpty()) deque.offer(arg);
-                else if (arg <= deque.peekFirst()) deque.offerFirst(arg);
-                else if (arg >= deque.peekLast()) deque.offerLast(arg);
+                dpq.insert(arg);
             } else if ("D".equals(op)) {
-                if (arg == -1 && !deque.isEmpty()) deque.pollFirst();
-                else if (arg == 1 && !deque.isEmpty()) deque.pollLast();
+                dpq.delete(arg);
             }
         }
         
-        return deque.isEmpty()
-            ? new int[]{0, 0}
-            : new int[]{deque.peekLast(), deque.peekFirst()};
+        return dpq.getValues();
     }
 }
 
+class DoublePriorityQueue {
+  Node head;
+  Node tail;
+
+  public DoublePriorityQueue() {}
+
+  public boolean insert(int value) {
+    if (head == null) {
+      head = new Node(value);
+      tail = head;
+      return true;
+    }
+
+    Node curNode = head;
+    Node prevNode = null;
+
+    while (curNode != null && curNode.value < value) {
+      prevNode = curNode;
+      curNode = curNode.next;
+    }
+
+    if (curNode == null) {
+      prevNode.next = new Node(value, prevNode, null);
+      tail = prevNode.next;
+      return true;
+    }
+
+    if (curNode.value == value) {
+      return false;
+    }
+
+    if (curNode == head) {
+      Node newNode = new Node(value, null, curNode);
+      curNode.prev = newNode;
+      head = newNode;
+      return true;
+    }
+
+    Node newNode = new Node(value, prevNode, curNode);
+    prevNode.next = newNode;
+    curNode.prev = newNode;
+
+    return true;
+  }
+
+  public boolean delete(int flag) {
+    if (head == null) {
+      return false;
+    }
+
+    if (head == tail) {
+      head.next = null;
+      tail.prev = null;
+      head = null;
+      tail = null;
+      return true;
+    }
+
+    if (flag == 1) {
+      Node removeTarget = tail;
+      int value = removeTarget.value;
+      tail = removeTarget.prev;
+      tail.next = null;
+      removeTarget.prev = null;
+      return true;
+    }
+
+    Node removeTarget = head;
+    head = removeTarget.next;
+    head.prev = null;
+    removeTarget.next = null;
+    return true;
+  }
+
+  public int[] getValues() {
+    if (head == null || tail == null) {
+      return new int[] {0, 0};
+    }
+
+    return new int[] {tail.value, head.value};
+  }
+
+  class Node {
+    int value;
+    Node prev;
+    Node next;
+
+    public Node(int value) {
+      this.value = value;
+    }
+
+    public Node(int value, Node next) {
+      this.value = value;
+      this.next = next;
+    }
+
+    public Node(int value, Node prev, Node next) {
+      this.value = value;
+      this.prev = prev;
+      this.next = next;
+    }
+  }
+}
