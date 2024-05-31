b9b85d0090694fde5cd58db4166ac5b309a9a2dc
YongHo Shin <gdtknight@naver.com>
2024-05-31 11:25:55 +0900

    [level 3] Title: 여행경로, Time: 0.38 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,105 +1,46 @@
-import java.lang.Comparable;
-
-import java.util.Stack;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.Map;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.Comparator;
 
 class Solution {
-    
     public String[] solution(String[][] tickets) {
         final String START = "ICN";
-        final int MAX_COUNT = tickets.length;
         
-        final Stack<String> answer = new Stack<>();
-        final Map<String, PriorityQueue<Ticket>> ticketMap = new HashMap<>();
-        final Set<Ticket> usedTickets = new HashSet<>();
+        final Map<String, PriorityQueue<String>> ticketMap = new HashMap<>();
+        String[] answer = new String[tickets.length + 1];
+        int idx = tickets.length;
         
         for (String[] ticket : tickets) {
             String depature = ticket[0];
             String arrival = ticket[1];
             
-            Ticket newTicket = new Ticket(depature, arrival);
+            PriorityQueue<String> arrivals = ticketMap.getOrDefault(depature, new PriorityQueue<String>());
             
-            PriorityQueue<Ticket> arrivals = ticketMap.getOrDefault(depature, new PriorityQueue<Ticket>());
-            
-            arrivals.offer(newTicket);
+            arrivals.offer(arrival);
             
             ticketMap.put(depature, arrivals);
         }
         
-        Stack<Ticket> stack = new Stack<>();
-        stack.push(ticketMap.get(START).peek());
-        usedTickets.add(ticketMap.get(START).peek());
+        Stack<String> stack = new Stack<>();
         
-        out :
+        stack.push(START);
+        
         while(!stack.isEmpty()) {
-            Ticket current = stack.peek();
+            String depature = stack.peek();
+            PriorityQueue<String> pq = ticketMap.get(depature);
             
-            Iterator<Ticket> it = ticketMap.getOrDefault(current, new PriorityQueue<Ticket>()).iterator();
-            
-            while(it.hasNext()) {
-                Ticket next = it.next();
-                if (usedTickets.add(next)) {
-                    stack.push(next);
-                    continue out;
-                }
+            if (pq != null && !pq.isEmpty()) {
+                stack.push(pq.poll());
+            } else {
+                answer[idx--] = stack.pop();
             }
-            
-            answer.push(current.arrival());
-            stack.pop();
         }
         
-        answer.push(START);
-        
-        return answer.stream().toArray(String[]::new);
-    }
-}
-    
-
-class Ticket implements Comparable<Ticket> {
-    String depature;
-    String arrival;
-    
-    Ticket (String depature, String arrival) {
-        this.depature = depature;
-        this.arrival = arrival;
-    }
-    
-    public String depature() {
-        return depature;
-    }
-    
-    public String arrival() {
-        return arrival;
-    }
-    
-    public int compareTo(Ticket t) {
-        return depature.equals(t.depature())
-            ? arrival.compareTo(t.arrival())
-            : depature.compareTo(t.depature());
-    }
-    
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        
-        if (!(o instanceof Ticket)) return false;
-        
-        Ticket t = (Ticket) o;
-        
-        return depature.equals(t.depature()) && arrival.equals(t.arrival());
-    }
-    
-    public int hashCode() {
-        return depature.hashCode() * 31 + arrival.hashCode();
-    }
-    
-    public String toString() {
-        return "[" + depature + ", " + arrival + "]";
+        return answer;
     }
 }
