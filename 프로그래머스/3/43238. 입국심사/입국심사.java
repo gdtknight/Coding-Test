50b479cd0a852c3af784c17f551db16371959567
YongHo Shin <gdtknight@naver.com>
2024-06-10 16:20:26 +0900

    [level 3] Title: 입국심사, Time: 10.44 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,30 +2,22 @@ import java.util.Arrays;
 
 class Solution {
     public long solution(int n, int[] times) {
-        int minTime = Integer.MAX_VALUE;
-        int maxTime = 0;
+        long min = 0;
+        long max = Arrays.stream(times).max().orElse(Integer.MAX_VALUE) * (long) n;
         
-        for (int time : times) {
-            minTime = Math.min(minTime, time);
-            maxTime = Math.max(maxTime, time);
-        }
-        
-        long min = (long) n / (long) times.length * (long) minTime;
-        long max = (long) n / (long) times.length * (long) maxTime;
-        
-        long mid;
-        long people = 0;
-        
-        while(min < max) {
-            mid = min + (max - min) / 2;
-            people = 0;
+        while(min <= max) {
+            long mid = min + (max - min) / 2;
+            
+            long people = 0;
             
             for (int time : times) {
-                people += mid / (long)time;
+                people += (mid / time);
             }
             
-            if (people < n)   min = mid + 1;
-            else              max = mid;
+            System.out.printf("Time : %d, People : %d%n", mid, people);
+            
+            if (people < n) min = mid + 1;
+            else max = mid - 1;
         }
         
         return min;
