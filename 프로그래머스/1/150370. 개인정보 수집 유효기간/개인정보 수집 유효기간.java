545ee6bd4f2efed2a48af74bd61d1c63c8f666c8
YongHo Shin <gdtknight@naver.com>
2024-06-29 15:54:49 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.29 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        Map<String, Integer> expMap = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        String[] dateArr = today.split("\\.");
+        long startDay = 
+            Integer.parseInt(dateArr[0]) * 12 * 28 + 
+            Integer.parseInt(dateArr[1]) * 28 + 
+            Integer.parseInt(dateArr[2]);
+        
+        for (String term : terms) {
+            String[] args = term.split(" ");
+            expMap.put(args[0], Integer.parseInt(args[1]) * 28);
+        }
+        
+        for (int idx = 0; idx < privacies.length; idx++) {
+            String[] args = privacies[idx].split(" ");
+            String privacyType = args[1];
+            
+            String[] dates = args[0].split("\\.");
+            
+            long endDay =
+                Integer.parseInt(dates[0]) * 12 * 28 + 
+                Integer.parseInt(dates[1]) * 28 + 
+                Integer.parseInt(dates[2])
+                + expMap.get(privacyType);
+            
+            if (startDay >= endDay) list.add(idx + 1);
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
