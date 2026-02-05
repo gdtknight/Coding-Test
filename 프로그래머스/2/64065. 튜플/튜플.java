c7994703e33b279c0689e01dfd3eec34caa2b1ce
Yoshin <yoshin@student.42gyeongsan.kr>
2026-02-05 18:27:28 +0900

    [level 2] Title: 튜플, Time: 60.73 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,34 +1,18 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String s) {
-        String[] tuples = s.substring(2, s.length() - 2).split("\\},\\{");
-        
-        List<List<Integer>> numList = new LinkedList<>();
-        List<Integer> answerList = new LinkedList<>();
-        
-        for (String tuple : tuples) {
-            List<Integer> nums = Arrays.stream(tuple.split(",")).map(Integer::parseInt).collect(Collectors.toList());
-            numList.add(nums);
-        }
-        numList.sort((l1, l2) -> Integer.compare(l1.size(), l2.size()));
-        // System.out.println(numList);
-        
-        for (int i = 0; i < numList.size(); i++) {
-            Integer cur = numList.get(i).get(0);
-            answerList.add(cur);
-            numList.get(i).remove(cur);
-            for (int idx = i + 1; idx < numList.size(); idx++) {
-                numList.get(idx).remove(cur);
-            }
-        }
-        int[] answer = new int[answerList.size()];
-        for (int idx = 0; idx < answer.length; idx++) {
-            answer[idx] = answerList.get(idx);
-        }
-        return answer;
+        return Arrays.stream(s.substring(2, s.length() - 2).split("\\},\\{"))
+                .map(t -> Arrays.stream(t.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList()))
+                .sorted(Comparator.comparingInt(List::size))
+                .collect(LinkedHashSet<Integer>::new,
+                         (set, list) -> list.forEach(set::add),
+                         LinkedHashSet::addAll)
+                .stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
